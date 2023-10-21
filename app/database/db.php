3a4c40d7de5e9d1846efaa50cedfdf7629a3bcbe
eleciii4estvo <gulevsky_iv@mail.ru>
2023-10-21 20:47:39 +0300

    Expansion of the database selection function

--- app/database/db.php
+++ app/database/db.php
@@ -9,18 +9,39 @@ function tt($value){
     echo '</pre>';
 };
 
-//Функция выбора всего из выбранной таблицы
-function selectAll($table){
-    global $pdo;
-    $sql = "SELECT * FROM $table";   //запрос
-    $query = $pdo->prepare($sql);   //Подготавливаем запрос
-    $query->execute();  //Выполняем запрос
+//Функция проверки на ошибки в подключении к БД
+function dbCheckError($query){
     $errInfo = $query->errorInfo();   //записываем сообщения об ошибках в переменную errorInfo
     if($errInfo[0]!==PDO::ERR_NONE){
         echo "Ошибка:".$errInfo;
         exit();
     };
-    return $query->fetchAll();
-}
+    return true;    //Возможно пригодится для дальнейшей логики
+};
 
-tt(selectAll('users'));
+//Функция выбора данных из одной таблицы
+function select($table, $selectAll=true, $params =[]){
+    global $pdo;
+    $sql = "SELECT * FROM $table";   //запрос
+    //Модификация запроса, если были заданы параметры
+    if(!empty($params)){    //проверяем, не пустой ли массив $params (задан ли он при вызове, так как изначально пустой)
+        $i=0;   //создаем инкремент для дальнейшей логики
+        foreach($params as $key => $value){
+            if(!is_numeric($value)){    //Если значение ключа не является числом, то мы обрамляем его в кавычки (например email)
+                $value = "'".$value."'";
+            };
+            if($i == 0){    //Если был задан один параметр то, к запросу добавится следующая запись
+                $sql = $sql . " WHERE $key = $value";
+            }else{          //Если параметров было больше одного, тогда такая
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;   //Увеличиваем инкремент каждую итерацию
+        };
+    };
+    $query = $pdo->prepare($sql);   //Подготавливаем запрос
+    $query->execute();  //Выполняем запрос
+    dbCheckError($query);
+    return $selectAll ? $query->fetchAll() : $query->fetch();    //Получать все или только одну запись?
+};
+
+tt(select('users'));
