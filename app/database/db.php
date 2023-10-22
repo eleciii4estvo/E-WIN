402b57f819348d6508df98f8a934577ce2a3a81e
eleciii4estvo <gulevsky_iv@mail.ru>
2023-10-22 03:53:53 +0300

    functions for deleting and modifying the database

--- app/database/db.php
+++ app/database/db.php
@@ -47,11 +47,12 @@ function select($table, $selectAll=true, $params =[], $custom=''){
     dbCheckError($query);
     return $selectAll ? $query->fetchAll() : $query->fetch();    //Получать все или только одну запись?
 };
+//Примеры использования
 // tt(select('users', true, [], "SELECT * FROM users WHERE money>500"));
 // tt(select('users'));
 
 //Функция записи в БД
-function insert($table, $params=[]){
+function insert($table, $params){
     global $pdo;
     $i=0;
     $columns ='';
@@ -67,17 +68,61 @@ function insert($table, $params=[]){
         $i++;
     }
     $sql = "INSERT INTO $table ($columns) VALUES ($mask)";   //запрос c заполнителями
+    //Для отладки 
     // tt($sql);
     // exit();
     $query = $pdo->prepare($sql);   //Подготавливаем запрос
     $query->execute($params);  //Выполняем запрос
     dbCheckError($query);
 };
+//для отладки insert
 // $arrData = [
 //     'admin'=>'0',
 //     'username'=>'Andedsrson',
 //     'email'=>'andadr@dropbox.com',
 //     'password'=>'sadKASdsadw231L',
 // ];
-
 //  insert('users', $arrData);
+
+//Обновление строки в таблице
+function update($table, $id, $params){
+    global $pdo;
+    $i=0;
+    $str ='';
+    foreach($params as $key => $value){
+        if($i==0){
+            $str = $str . "$key = '$value'";
+        } else{
+            $str = $str . ", $key = '$value'";
+        }
+        $i++;
+    }
+    $sql = "UPDATE $table SET $str WHERE id = $id";   //запрос
+    //отладка
+    // tt($sql);
+    // exit();
+    $query = $pdo->prepare($sql);   //Подготавливаем запрос
+    $query->execute();  //Выполняем запрос
+    dbCheckError($query);
+};
+//для отладки update
+// $arrData=[
+//     'admin'=>'1',
+//     'username'=>'olakOslw',
+//     'money'=>500,
+// ];
+// update('users', 3, $arrData);
+
+//Удаление строки в таблице
+function delete($table, $id){
+    global $pdo;
+    $sql = "DELETE FROM $table WHERE id = $id";   //запрос
+    //отладка
+    // tt($sql);
+    // exit();
+    $query = $pdo->prepare($sql);   //Подготавливаем запрос
+    $query->execute();  //Выполняем запрос
+    dbCheckError($query);
+};
+
+delete('users', 3);
